c941d961c89421b77adc74360549888c41737a81
zilmar <zilmar@pj64-emu.com>
2015-10-23 08:23:11 +1100

    [Project64] Add basic console project

---
+++ Source/Project64-console/main.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+
+int main(int /*argc*/, char * /*argv*/ [])
+{
+    printf("    ____               _           __  _____ __ __\n");
+    printf("   / __ \\_________    (_)__  _____/ /_/ ___// // /\n");
+    printf("  / /_/ / ___/ __ \\  / / _ \\/ ___/ __/ __ \\/ // /_\n");
+    printf(" / ____/ /  / /_/ / / /  __/ /__/ /_/ /_/ /__  __/\n");
+    printf("/_/   /_/   \\____/_/ /\\___/\\___/\\__/\\____/  /_/   \n");
+    printf("                /___/                             \n");
+    printf("http://www.pj64-emu.com/\n");
+    printf("%s Version %s\n\n", VER_FILE_DESCRIPTION_STR, VER_FILE_VERSION_STR);
+}
