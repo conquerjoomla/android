1daa791d83e1f051527bb311bdfc1a387af78011
zilmar <zilmar@pj64-emu.com>
2015-11-20 16:18:19 +1100

    [Android] Add scan_roms_activity.xml

--- Source/Android/src/emu/project64/ActivityHelper.java
+++ Source/Android/src/emu/project64/ActivityHelper.java
@@ -14,12 +14,29 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.TextUtils;
 
 /**
  * Utility class that encapsulates and standardizes interactions between activities.
  */
 public class ActivityHelper
 {
+    /**
+     * Keys used to pass data to activities via the intent extras bundle. It's good practice to
+     * namespace the keys to avoid conflicts with other apps. By convention this is usually the
+     * package name but it's not a strict requirement. We'll use the fully qualified name of this
+     * class since it's easy to get.
+     */
+    public static class Keys
+    {
+        private static final String NAMESPACE = Keys.class.getCanonicalName() + ".";
+        //@formatter:off
+        public static final String ROM_PATH             = NAMESPACE + "ROM_PATH";
+        public static final String SEARCH_PATH          = NAMESPACE + "GALLERY_SEARCH_PATH";
+        public static final String CLEAR_GALLERY        = NAMESPACE + "GALLERY_CLEAR_GALLERY";
+        //@formatter:on
+    }
+    
     public static final int SCAN_ROM_REQUEST_CODE = 1;
     
     public static void startGalleryActivity( Context context, Uri romPath )
@@ -30,6 +47,10 @@ public class ActivityHelper
     public static void startGalleryActivity( Context context, String romPath )
     {
         Intent intent = new Intent( context, GalleryActivity.class );
+        if( !TextUtils.isEmpty( romPath ) )
+        {
+            intent.putExtra( ActivityHelper.Keys.ROM_PATH, romPath );
+        }
         context.startActivity( intent );
     }
     
