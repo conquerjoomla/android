2e58c9c0bf0c5e55d371816ccba2b9c7c4fe707c
zilmar <zilmar@pj64-emu.com>
2015-11-27 09:32:52 +1100

    [Android] Remove Object files

--- Source/Android/jni/Android.mk
+++ Source/Android/jni/Android.mk
@@ -1,11 +1,7 @@
 JNI_LOCAL_PATH := $(call my-dir)
 
 AE_BRIDGE_INCLUDES := $(JNI_LOCAL_PATH)/ae-bridge/
-M64P_API_INCLUDES := $(JNI_LOCAL_PATH)/mupen64plus-core/src/api/
 SDL_INCLUDES := $(JNI_LOCAL_PATH)/SDL2/include/
-PNG_INCLUDES := $(JNI_LOCAL_PATH)/png/include/
-SAMPLERATE_INCLUDES := $(JNI_LOCAL_PATH)/libsamplerate/
-FREETYPE_INCLUDES := $(JNI_LOCAL_PATH)/freetype/include/
 
 COMMON_CFLAGS :=                    \
     -O3                             \
