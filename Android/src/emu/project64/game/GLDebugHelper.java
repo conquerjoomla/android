198cec775416a74b53246d965078ab576d6da173
zilmar <zilmar@pj64-emu.com>
2016-02-25 13:58:49 +1100

    Remove GLErrorWrapper.java

--- Android/src/emu/project64/game/GLDebugHelper.java
+++ Android/src/emu/project64/game/GLDebugHelper.java
@@ -77,14 +77,6 @@ public class GLDebugHelper {
     public static final int ERROR_WRONG_THREAD = 0x7000;
 
     public static GL wrap(GL gl, int configFlags, Writer log) {
-        if ( configFlags != 0 ) {
-            gl = new GLErrorWrapper(gl, configFlags);
-        }
-        if ( log != null ) {
-            boolean logArgumentNames =
-                (CONFIG_LOG_ARGUMENT_NAMES & configFlags) != 0;
-            gl = new GLLogWrapper(gl, log, logArgumentNames);
-        }
         return gl;
     }
 }
