d54e11206e5160db06dacccf069a80d78a91643e
zilmar <zilmar@pj64-emu.com>
2016-02-25 13:46:36 +1100

    Remove logging from JavaBridge.cpp

--- Source/Android/Bridge/JavaBridge.cpp
+++ Source/Android/Bridge/JavaBridge.cpp
@@ -12,8 +12,6 @@
 #include "jniBridge.h"
 
 #ifdef ANDROID
-#include <android/log.h>
-
 JavaBridge::JavaBridge(JavaVM* vm) :
 	m_vm(vm)
 {
@@ -21,33 +19,22 @@ JavaBridge::JavaBridge(JavaVM* vm) :
 
 void JavaBridge::GfxThreadInit()
 {
-    __android_log_print(ANDROID_LOG_ERROR, "jniBridge", "JavaBridge::GfxThreadInit");
     JNIEnv *env = Android_JNI_GetEnv();
-    __android_log_print(ANDROID_LOG_ERROR, "jniBridge", "JavaBridge::GfxThreadInit env = %p",env);
 	if (g_GLThread != NULL)
 	{
 		jclass GLThreadClass = env->GetObjectClass(g_GLThread);
-		__android_log_print(ANDROID_LOG_ERROR, "jniBridge", "JavaBridge::GfxThreadInit GLThreadClass = %p",GLThreadClass);
 		jmethodID midThreadStarting = env->GetMethodID(GLThreadClass, "ThreadStarting", "()V");
-		__android_log_print(ANDROID_LOG_ERROR, "jniBridge", "JavaBridge::GfxThreadInit midThreadStarting = %p",midThreadStarting);
 		env->CallVoidMethod(g_GLThread, midThreadStarting);
-		__android_log_print(ANDROID_LOG_ERROR, "jniBridge", "JavaBridge::GfxThreadInit Called?");
 		env->DeleteLocalRef(GLThreadClass);
-	}
-	else
-	{
-		__android_log_print(ANDROID_LOG_ERROR, "jniBridge", "g_GLThread == NULL");
 	}
 }
 
 void JavaBridge::GfxThreadDone()
 {
-    __android_log_print(ANDROID_LOG_ERROR, "jniBridge", "JavaBridge::GfxThreadDone");
 }
 
 void JavaBridge::SwapWindow()
 {
-    __android_log_print(ANDROID_LOG_ERROR, "jniBridge", "JavaBridge::SwapWindow");
     JNIEnv *env = Android_JNI_GetEnv();
 	if (g_GLThread != NULL)
 	{
@@ -55,10 +42,6 @@ void JavaBridge::SwapWindow()
 		jmethodID midSwapBuffers = env->GetMethodID(GLThreadClass, "SwapBuffers", "()V");
 		env->CallVoidMethod(g_GLThread, midSwapBuffers);
 		env->DeleteLocalRef(GLThreadClass);
-	}
-	else
-	{
-		__android_log_print(ANDROID_LOG_ERROR, "jniBridge", "g_GLThread == NULL");
 	}
 }
 
